8fc22667367d7b6c118d7ccc4bed78df857fd1c2
Nripesh Rajbhandari <nripesh.rajbhandari@gmail.com>
2024-05-20 01:42:29 +0100

    modified App.js and Budgets.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import Transactions from '../features/transactions/Transactions';
+import Budgets from '../features/budgets/Budgets';
+import React from 'react';
 
 function App() {
   return (
-    <div>
-      
+    <div className="App">
+      <header className="App-header">
+        <h1>Expense Tracker</h1>
+        <Budgets />
+        <Transactions />
+      </header>
     </div>
   );
 }
